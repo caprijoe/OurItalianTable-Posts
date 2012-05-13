2c857b0944c814094481762e5a0149040ca6e1c1
Joseph Becci <jcbecci@Josephs-MacBook-Air.local>
2012-05-13 13:44:57 -0700

    Initial Commit

---
+++ OurItalianTable Posts/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  OurItalianTable Posts
+//
+//  Created by Joseph Becci on 5/13/12.
+//  Copyright (c) 2012 Our Italian Table. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "oitPostsAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([oitPostsAppDelegate class]));
+    }
+}
---
+++ OurItalianTable Posts/oitPostsAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  oitPostsAppDelegate.h
+//  OurItalianTable Posts
+//
+//  Created by Joseph Becci on 5/13/12.
+//  Copyright (c) 2012 Our Italian Table. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface oitPostsAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
