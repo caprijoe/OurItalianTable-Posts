939d0ee62e11ef4db48407e316bc9ca83aeb960b
Joseph Becci <jcbecci@Josephs-MacBook-Air.local>
2012-10-11 20:07:55 -0700

    Add nav controller to handle rotation

---
+++ OurItalianTable Posts/OITNavController.h
@@ -0,0 +1,13 @@
+//
+//  OITNavController.h
+//  OurItalianTable Posts
+//
+//  Created by Joseph Becci on 10/11/12.
+//  Copyright (c) 2012 Our Italian Table. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OITNavController : UINavigationController
+
+@end
