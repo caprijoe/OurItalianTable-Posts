f30251c7cb8a02cac831f89786d95393c326927e
Joseph Becci <jcbecci@Josephs-MacBook-Air.local>
2012-12-28 18:12:26 -0800

    updated app delegate name

--- OurItalianTable Posts/main.m
+++ OurItalianTable Posts/main.m
@@ -8,11 +8,11 @@
 
 #import <UIKit/UIKit.h>
 
-#import "oitPostsAppDelegate.h"
+#import "AppDelegate.h"
 
 int main(int argc, char *argv[])
 {
     @autoreleasepool {
-        return UIApplicationMain(argc, argv, nil, NSStringFromClass([oitPostsAppDelegate class]));
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
     }
 }
