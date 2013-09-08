58b434dfe3bcc329c4cc575abc4af03ba078566e
Joe Becci <jcbecci@gmail.com>
2013-09-08 15:30:04 -0700

    Final commit before testing

--- OurItalianTable Posts/postRecord.h
+++ OurItalianTable Posts/postRecord.h
@@ -13,6 +13,7 @@
 @property (nonatomic, strong) NSString *postURLString;
 @property (nonatomic, strong) NSString *postHTML;
 @property (nonatomic, strong) NSDate   *postPubDate;
+@property (nonatomic, strong) NSString *postPubDateAsString;
 @property (nonatomic, strong) NSMutableArray *postCategories;
 @property (nonatomic, strong) NSMutableArray *postTags;
 @property (nonatomic, strong) NSNumber *latitude;
