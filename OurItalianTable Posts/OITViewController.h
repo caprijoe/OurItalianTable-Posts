b9946a04205a40c036c57df985d8f3ae42a5d28b
Joe Becci <jcbecci@gmail.com>
2013-12-01 13:26:13 -0800

    initial commit

---
+++ OurItalianTable Posts/OITViewController.h
@@ -0,0 +1,13 @@
+//
+//  OITViewController.h
+//  OurItalianTable Posts
+//
+//  Created by Joseph Becci on 12/1/13.
+//  Copyright (c) 2013 Our Italian Table. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OITViewController : UIViewController
+
+@end
---
+++ OurItalianTable Posts/OITViewController.m
@@ -0,0 +1,34 @@
+//
+//  OITViewController.m
+//  OurItalianTable Posts
+//
+//  Created by Joseph Becci on 12/1/13.
+//  Copyright (c) 2013 Our Italian Table. All rights reserved.
+//
+
+#import "OITViewController.h"
+
+@interface OITViewController ()
+
+@end
+
+@implementation OITViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+
+    // support for change of perferred text font and size
+    [[NSNotificationCenter defaultCenter] addObserver:self selector:@selector(preferredContentSizeChanged:) name:UIContentSizeCategoryDidChangeNotification object:nil];
+
+}
+
+// Must be overridden, abstract method to handle text change
+- (void)preferredContentSizeChanged:(NSNotification *)aNotification {
+    // adjust the layout of the cells
+    [self.view setNeedsLayout];
+    
+    // refresh view...
+}
+
+@end
